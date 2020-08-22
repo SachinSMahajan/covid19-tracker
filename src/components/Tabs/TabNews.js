e1882d87263a76d3bf590616efdd240a57db9e2d
sachin <sachinmahajan712@gmail.com>
2020-08-22 19:31:25 +0530

    api change

--- src/components/Tabs/TabNews.js
+++ src/components/Tabs/TabNews.js
@@ -4,7 +4,7 @@ import NewsItem from '../DataDisplay/NewsItem'
 import './TabNews.css'
 function TabNews (props){
     const [News, setNews] = useState([])
-    const APIKey = 'APT-KEY';
+    const APIKey = 'xX69Wv-vHOe0dOaIx2Xfzudj_zWE15ls1fiMJW-1h6MjAW8T';
     useEffect(()=>{
         const getNews = async ()=>{
             setNews(await FetchNews())
@@ -13,7 +13,8 @@ function TabNews (props){
     },[])
 
     const FetchNews =async ()=>{
-        const response = await axios('https://newsapi.org/v2/top-headlines?q=covid&country=in&apiKey=' + APIKey)
+        //const response = await axios('https://newsapi.org/v2/top-headlines?q=covid&country=in&apiKey=' + APIKey)
+        const response = await axios('https://api.currentsapi.services/v1/search?q=coronavirus&q=covid19&language=en&apiKey=' + APIKey)
     //     const newslist =[
     //         {"urlToImage":'./test.png',
     //     "description" : "How to use chart.js to create charts in React",
