2844d756ca08a70559738eafb5f9a0c2e49812f2
Sachin S Mahajan <sachinmahajan712@gmail.com>
2020-08-23 15:52:22 +0530

    update

--- src/components/Tabs/TabNews.js
+++ src/components/Tabs/TabNews.js
@@ -4,7 +4,7 @@ import NewsItem from '../DataDisplay/NewsItem'
 import './TabNews.css'
 function TabNews (props){
     const [News, setNews] = useState([])
-    const APIKey = 'xX69Wv-vHOe0dOaIx2Xfzudj_zWE15ls1fiMJW-1h6MjAW8T';
+    const APIKey = 'PUT API_KEY';
     useEffect(()=>{
         const getNews = async ()=>{
             setNews(await FetchNews())
@@ -15,15 +15,8 @@ function TabNews (props){
     const FetchNews =async ()=>{
         //const response = await axios('https://newsapi.org/v2/top-headlines?q=covid&country=in&apiKey=' + APIKey)
         const response = await axios('https://api.currentsapi.services/v1/search?keywords=corona&keyword=covid19&language=en&country=IN&apiKey=' + APIKey)
-    //     const newslist =[
-    //         {"urlToImage":'./test.png',
-    //     "description" : "How to use chart.js to create charts in React",
-    // "content":"The state variable contains all the data and styling properties of the bar graph. The labels keyword assigns names to each bar, and the dataset sub-set contains information such as bar color, border width, ​and height of the bar"}
-    //     ]
          return response.data.news
-        //return  newslist;
     }
-
     return (
      <>
         <NewsItem data={News}></NewsItem>
