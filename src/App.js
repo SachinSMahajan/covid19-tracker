95c0f76591e85f2c58f151e40a18238198c228bc
Sachin Mahajan <sachinmahajan712@gmail.com>
2021-03-13 10:46:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header'
-import  MainTabs from './components/Tabs/MainTabs'
+import MainTabs from './components/Tabs/MainTabs'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
